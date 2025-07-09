1c7a6e897de6e381ded953d6c44d8a76ecd8e670
Muhammad Majid <mmajidkamel@gmail.com>
2025-07-09 02:23:59 +0200

    corrected region in vpc

--- 0_foundation/main.tf
+++ 0_foundation/main.tf
@@ -22,7 +22,7 @@ module "vpc" {
   name = local.cluster_name
   cidr = "10.0.0.0/16" # group of IPs in our network (vpc)
 
-  azs             = ["eu-north-1a", "eu-north-1b", "eu-north-1c"]
+  azs             = ["eu-central-1a", "eu-central-1b", "eu-central-1c"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
   intra_subnets   = ["10.0.51.0/24", "10.0.52.0/24", "10.0.53.0/24"]
